30ec7b11821a077bdaa1960cde8d987aeeea45d5
shabang <shanila866@gmail.com>
2021-12-20 12:46:35 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# React Widget E-Commerce Store
+
+The React Widget Store is an E-Commerce site made with React and Redux for the frontend and Ruby on Rails for the backend. The site has full user support with registering and signing in. A user can view, review, and purchase items! A user will add items to their cart, proceed through the checkout wizard and place an order that is persisted to the backend.
+
+## Preview of Homepage
+<img src="https://i.imgur.com/JA4inMl.png" />
+
+## Preview of Product Page
+<img src="https://i.imgur.com/6PZHacJ.png" />
+
+## Preview of Create A Product Modal
+<img src="https://i.imgur.com/Dc8V7Wp.png" />
+
+## Usage
+
+**NOTE: It is required to get the [backend](https://github.com/Xearta/react-ecommerce-backend) and follow those instructions as well.**
+This application is currently only available to run locally.
+
+First clone this repo and run the following commands:
+
+```
+npm install
+npm start
+```
+
+If everything runs smoothly, the web site should start up at `http://localhost:3000`. You will need to get the [backend](https://github.com/Xearta/react-ecommerce-backend) and follow the instructions there to get the server running.
+
+## Contributing
+
+Bug reports and pull requests are welcome on GitHub at https://github.com/Xearta/react-ecommerce-frontend. This project is intended to be a safe, welcoming space for collaboration, and contributors are expected to adhere to the Contributor Covenant code of conduct.
+
+## License
+
+The app is available as open source under the terms of the MIT License.
---
+++ src/App.js
@@ -0,0 +1,153 @@
+import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { filterProductsByCategory } from "./actions/productActions";
+// Main Containers
+import HomeContainer from "./containers/HomeContainer";
+import ProductContainer from "./containers/ProductContainer";
+// Cart & Checkout Containers
+import CartContainer from "./containers/checkoutContainers/CartContainer";
+import ShippingContainer from "./containers/checkoutContainers/ShippingContainer";
+import PaymentContainer from "./containers/checkoutContainers/PaymentContainer";
+import PlaceOrderContainer from "./containers/checkoutContainers/PlaceOrderContainer";
+import OrderContainer from "./containers/checkoutContainers/OrderContainer";
+// User Containers
+import SigninContainer from "./containers/userContainers/SigninContainer";
+import RegisterContainer from "./containers/userContainers/RegisterContainer";
+import ProfileContainer from "./containers/userContainers/ProfileContainer";
+// Admin Containers
+import ProductsContainer from "./containers/adminContainers/ProductsContainer";
+import OrdersContainer from "./containers/adminContainers/OrdersContainer";
+
+function App() {
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.user.userInfo);
+  const { products } = useSelector((state) => state.products);
+  const { cartItems } = useSelector((state) => state.cart);
+
+  const openMenu = () => {
+    document.querySelector(".sidebar").classList.add("open");
+  };
+
+  const closeMenu = () => {
+    document.querySelector(".sidebar").classList.remove("open");
+  };
+
+  const categoryHandler = (category) => {
+    closeMenu();
+    dispatch(filterProductsByCategory(products, category));
+  };
+
+  return (
+    <BrowserRouter>
+      <div className="grid-container">
+        {/* Header Section */}
+        <header className="header">
+          <div className="brand">
+            <button onClick={openMenu}>&#9776;</button>
+            <Link to="/">shabang Store</Link>
+          </div>
+          <div className="header-links">
+            <Link to="/cart">
+              Cart{" "}
+              {cartItems.length > 0
+                ? `(Items: ${cartItems.reduce((a, c) => a + c.qty, 0)})`
+                : "is empty"}
+            </Link>
+            {user ? (
+              <Link to="/profile">{user.first_name}</Link>
+            ) : (
+              <Link to="/signin">Sign In</Link>
+            )}
+            {user && user.isAdmin && (
+              <div className="dropdown">
+                <Link to="/orders">Admin</Link>
+                <ul className="dropdown-content">
+                  <li>
+                    <Link to="/orders">Orders</Link>
+                    <Link to="/products">Products</Link>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
+        </header>
+
+        {/* Category Section */}
+        <aside className="sidebar">
+          <h3>Shopping Categories</h3>
+          <button className="sidebar-close-button" onClick={closeMenu}>
+            X
+          </button>
+          <ul className="categories">
+            <li>
+              <button
+                type="button"
+                onClick={(e) => categoryHandler(e.target.innerText)}
+              >
+                All
+              </button>
+            </li>
+            <li>
+              <button
+                type="button"
+                onClick={(e) => categoryHandler(e.target.innerText)}
+              >
+               fruit
+              </button>
+            </li>
+            <li>
+              <button
+                type="button"
+                onClick={(e) => categoryHandler(e.target.innerText)}
+              >
+                vegetable
+              </button>
+            </li>
+            <li>
+              <button
+                type="button"
+                onClick={(e) => categoryHandler(e.target.innerText)}
+              >
+                rice
+              </button>
+            </li>
+            <li>
+              <button
+                type="button"
+                onClick={(e) => categoryHandler(e.target.innerText)}
+              >
+               tea
+              </button>
+            </li>
+          </ul>
+        </aside>
+
+        {/* Main Content Section */}
+        <div className="content">
+          <main className="main">
+            <Route path="/register" component={RegisterContainer} />
+            <Route path="/signin" component={SigninContainer} />
+            <Route path="/profile" component={ProfileContainer} />
+            <Route path="/products" exact component={ProductsContainer} />
+            <Route path="/shipping" component={ShippingContainer} />
+            <Route path="/payment" component={PaymentContainer} />
+            <Route path="/placeorder" component={PlaceOrderContainer} />
+            <Route path="/orders" exact component={OrdersContainer} />
+            <Route path="/orders/:id" component={OrderContainer} />
+            <Route path="/products/:id" exact component={ProductContainer} />
+            <Route path="/cart/:id?" component={CartContainer} />
+            <Route path="/" exact component={HomeContainer} />
+          </main>
+        </div>
+
+        {/* Footer */}
+        <footer className="footer">
+          Copyright shabang. All rights reserved.
+        </footer>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
